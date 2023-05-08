bb22d6ede95710885ded4a567d6669d90df6c044
Gabriel Lages <gabriellages2@gmail.com>
2023-05-08 14:29:33 -0300

    Novos sistemas
    -Adicionado sistema de movimentação do player
    -Adicionado sistema de movimentação da camera

---
+++ Prova CG/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float velocidade = 5;
+    public float velocidade_de_corrida=10;
+    public KeyCode tecla_de_corrida= KeyCode.LeftShift;
+    public Animator anim;
+
+    private Rigidbody rb;
+    public float velocidade_atual;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        velocidade_atual=velocidade;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var inputX = Input.GetAxis("Horizontal");
+        var inputZ = Input.GetAxis("Vertical");
+
+        if (inputX != 0 || inputZ != 0) 
+        {
+            transform.Translate(new Vector3(inputX, 0, inputZ) * Time.deltaTime * velocidade_atual, Space.Self);
+
+            if (Input.GetKey(tecla_de_corrida))
+            {
+                anim.SetBool("run", true);
+                velocidade_atual = velocidade_de_corrida;
+            }
+            else
+            {
+                anim.SetBool("run", false);
+                velocidade_atual = velocidade;
+            }
+        }
+        else
+        {
+            anim.SetBool("run", false);
+            velocidade_atual = velocidade;
+        }
+  
+
+
+    }
+}
