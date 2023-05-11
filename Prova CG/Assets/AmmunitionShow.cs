c4c9afd95535384d4fbbb18636b8909d4f4f4ad7
DarcountBR <gugaloki37@gmail.com>
2023-05-10 22:38:02 -0300

    Mudança
    Aqui, tirei o AmmunitionShow text;
    que estava dando erro de compilação
    -Necessário entender qual o erro, para que a munição possa aparecer no HUD

--- Prova CG/Assets/AmmunitionShow.cs
+++ Prova CG/Assets/AmmunitionShow.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class AmmunitionShow : MonoBehaviour
 {
 
     public int municao;
     public bool atirando;
-    public text ammunitionShow;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +18,7 @@ public class AmmunitionShow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ammunitionShow.text = municao.ToString();
+        
         if(Input.GetMouseButtonDown(0) && !atirando && municao > 0)
         {
                atirando = true;
