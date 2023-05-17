167aca8b2bfd9f370e42ded40a246a3d5f41fb23
DarcountBR <gugaloki37@gmail.com>
2023-05-17 16:42:08 -0300

    Adicionado efeito de impacto
    Efeito de Impacto adicionado aos projéteis, quando eles colidem com algum objeto.

--- Prova CG/Assets/Scripts/projetilcollider.cs
+++ Prova CG/Assets/Scripts/projetilcollider.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class projetilcollider : MonoBehaviour // sistema para colisão de projéteis
+public class projetilcollider : MonoBehaviour // sistema para colisão de projéteis e o seu impacto
 {
+    public GameObject impactoVFX;
+   
     private bool collided;
     void OnCollisionEnter(Collision co)
     {
         if(co.gameObject.tag != "Bullet" && co.gameObject.tag != "Player")
         {
             collided = true;
+
+            var impact = Instantiate (impactoVFX, co.contacts[0].point, Quaternion.identity) as GameObject;
+
+            Destroy (impact, 2);
+
             Destroy (gameObject);   
         }
     }
