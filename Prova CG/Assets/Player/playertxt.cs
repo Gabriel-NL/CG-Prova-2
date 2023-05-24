9af84b70c2f41c29cb8bb6309ef476dc05b59b58
DarcountBR <gugaloki37@gmail.com>
2023-05-24 16:56:15 -0300

    Adicionado pontuação
    Adicionado sistema básico de pontuação
    -Caso você aperte P, receberá 100 pontos
    -Caso aperte O, receberá 50

--- Prova CG/Assets/Player/playertxt.cs
+++ Prova CG/Assets/Player/playertxt.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class playertxt : MonoBehaviour
 {
+    public playerpoints score;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class playertxt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            score.AddScore(100);
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            score.AddScore(50);
+        }
     }
 }
