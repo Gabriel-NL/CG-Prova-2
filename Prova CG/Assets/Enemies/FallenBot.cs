688b58d1eaa01fc6eb4f41159f0b02acfe9276f0
Gabriel Lages <gabriellages2@gmail.com>
2023-05-25 10:29:05 -0300

    Trying to fix root motion gravity, failed

--- Prova CG/Assets/Enemies/FallenBot.cs
+++ Prova CG/Assets/Enemies/FallenBot.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class FallenBot : MonoBehaviour
 {
+
+    public Animator animator;
+    public bool grounded;
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
+    private void OnCollisionEnter(Collision c)
+    {
+
+        if (c.gameObject.tag == "GroundTag")
+        {
+            grounded = true;
+            //animator.applyRootMotion = true;
+
+        }
+    }
+    private void OnCollisionExit(Collision c)
+    {
+        if (c.gameObject.tag == "GroundTag")
+        {
+            grounded = false;
+            animator.applyRootMotion = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(Vector3.down * 300);
     }
+
+
 }
