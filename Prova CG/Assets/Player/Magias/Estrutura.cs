4be6776a31dcab7f33d3a187da0da296786fa22e
Gabriel Lages <gabriellages2@gmail.com>
2023-05-29 19:43:09 -0300

    Classe com todas as magias implementada

---
+++ Prova CG/Assets/Player/Magias/Estrutura.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estrutura
+{
+    public string Nome { get; set; }
+    public int Dano { get; set; }
+    public int DanoNaCabeca { get; set; }
+    public bool DanoEmArea { get; set; }
+    public double Cadencia { get; set; }
+    public int Usos { get; set; }
+
+    public GameObject Vfx { get; set; }
+
+    public Estrutura(string nome,int dano, int danoNaCabeca, bool danoEmArea, double cadencia, int usos, GameObject vfx)
+    {
+        this.Nome = nome;
+        this.Dano = dano;
+        this.DanoNaCabeca = danoNaCabeca;
+        this.DanoEmArea = danoEmArea;
+        this.Cadencia = cadencia;
+        this.Usos = usos;
+        this.Vfx = vfx;
+
+}
+
+}
