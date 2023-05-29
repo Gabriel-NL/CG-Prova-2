248386a990d7f8c5cfeff66630514f1dbaf20efe
Gabriel Lages <gabriellages2@gmail.com>
2023-05-29 18:33:27 -0300

    Correcao

--- Prova CG/Assets/Player/Disparo.cs
+++ Prova CG/Assets/Player/Disparo.cs
@@ -19,34 +19,21 @@ public class Disparo : MonoBehaviour
         }
     }
 
-    public Camera cam;
-    public GameObject projectile;
-    public Transform RHFirePoint;
     public float projectileSpeed = 30;
     public float cadencia = 4;
     public float arcRange = 1;
 
+
+    public GameObject projectile;
+    public Transform RHFirePoint;
     private Vector3 destination;
-    private float contador_disparo;
+    private float cronometro;
 
-    public PlayerData pd;
-
-    public void PlayerAmmoSystem(TMP_Text mostrarMunicao, PlayerData pd)
-    {
-        this.municao = pd.getCargas();
-        mostrarMunicao.text = "Cargas: " + this.municao;
-        if (Input.GetMouseButtonDown(0) && !this.atirando && pd.getCargas() > 0)
-        {
-            this.atirando = true;
-            this.atirando = false;
-        }
-    }
-
-    public void CastingSystem()
+    public void CastingSystem(PlayerData pd, Camera cam)
     {
         if (Input.GetButton("Fire1") && pd.getCargas() > 0)
         {
-            contador_disparo = Time.time + 1 / cadencia;
+            cronometro = Time.time + 1 / cadencia;
             pd.consumirCarga();
 
             Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
@@ -57,15 +44,11 @@ public class Disparo : MonoBehaviour
             else
                 destination = ray.GetPoint(1000);
 
-            ProjectileSystem(RHFirePoint);
+            var projectileObj = Instantiate(projectile, RHFirePoint.position, Quaternion.identity) as GameObject;
+            projectileObj.GetComponent<Rigidbody>().velocity = (destination - RHFirePoint.position).normalized * projectileSpeed;
+
+            iTween.PunchPosition(projectileObj, new Vector3(Random.Range(-arcRange, arcRange), Random.Range(-arcRange, arcRange), 0), Random.Range(0.5f, 2));
         }
     }
 
-    void ProjectileSystem(Transform firePoint)
-    {
-        var projectileObj = Instantiate(projectile, firePoint.position, Quaternion.identity) as GameObject;
-        projectileObj.GetComponent<Rigidbody>().velocity = (destination - firePoint.position).normalized * projectileSpeed;
-
-        iTween.PunchPosition(projectileObj, new Vector3(Random.Range(-arcRange, arcRange), Random.Range(-arcRange, arcRange), 0), Random.Range(0.5f, 2));
-    }
 }
