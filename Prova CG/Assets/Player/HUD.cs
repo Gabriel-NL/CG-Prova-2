8b4271371c5aaea3a2ae1ed208cceece79cccf1b
DarcountBR <gugaloki37@gmail.com>
2023-06-18 15:34:31 -0300

    HUD com as magias
    Agora, a magia selecionada(Seu símbolo) irá aparecer na tela

--- Prova CG/Assets/Player/HUD.cs
+++ Prova CG/Assets/Player/HUD.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HUD : MonoBehaviour
+public class HUDController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image iconImage;
+    public Sprite[] icons;
+
+    private int currentIconIndex = 0;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentIconIndex = 0;
+            UpdateIcon();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentIconIndex = 1;
+            UpdateIcon();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentIconIndex = 2;
+            UpdateIcon();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currentIconIndex = 3;
+            UpdateIcon();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateIcon()
     {
-        
+        if (currentIconIndex >= 0 && currentIconIndex < icons.Length)
+        {
+            iconImage.sprite = icons[currentIconIndex];
+        }
     }
 }
